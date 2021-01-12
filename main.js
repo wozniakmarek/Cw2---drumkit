903f3d53955f4f8e3b26ad9ffefaa1c537927258
BoskiMarecki <72198070+BoskiMarecki@users.noreply.github.com>
2021-01-12 20:29:09 +0100

    DONE
    DONE

--- main.js
+++ main.js
@@ -15,18 +15,18 @@ let soundList = {
     i: "tom"
 }
 
-function loadAudio() { //wczytanie d�wi�k�w
+function loadAudio() {
     Object.values(soundList).forEach((e) => sound[e] = new Audio('./sounds/' + e + '.wav'));
 }
 
-function keyDown(ev) { // nacisniecie klawisza
+function keyDown(ev) {
     if (document.getElementById(ev.key)) {
-        document.getElementById(ev.key).classList.add('key-pressed') //jesli element istnieje, to dodaje do niego klase
+        document.getElementById(ev.key).classList.add('key-pressed')
         playKey(ev);
     }
 }
 
-function keyUp(ev) { // puszczenie klawisza
+function keyUp(ev) { 
     if (document.getElementById(ev.key)) {
         document.getElementById(ev.key).classList.remove('key-pressed')
     }
@@ -36,28 +36,28 @@ function keyUp(ev) { // puszczenie klawisza
 
 
 
-function startRecord(numer_sciezki) { // nagrywanie sciezki
-    if (!record[numer_sciezki]) { // jesli nie ma sciezki o danym numerze, tworzy j� z aktualnym czasem, zaczyna nagrywa� i zmienia napis na przycisku
+function startRecord(numer_sciezki) {
+    if (!record[numer_sciezki]) {
         record[numer_sciezki] = true;
         time[numer_sciezki] = Date.now();
         sciezka[numer_sciezki] = [];
         document.getElementById("rButton" + numer_sciezki).innerText = "Stop recording";
 
     }
-    else { // jesli istnieje sciezka, to przestaje j� nagrywa� i wyswietla informacje o sciezce.
+    else {
         record[numer_sciezki] = false;
         document.getElementById("rButton" + numer_sciezki).innerText = "Record track " + numer_sciezki;
         document.getElementById("status" + numer_sciezki).innerText = sciezka[numer_sciezki].length + " Sounds recorded in " + Math.round(sciezka[numer_sciezki][sciezka[numer_sciezki].length-1].delay/100)/10+"sec";
     }
 }
-function playKey(ev) { // funkcja grania muzyki
-    sound[soundList[ev.key]].currentTime = 0; // zeruje czas dzwieku
+function playKey(ev) {
+    sound[soundList[ev.key]].currentTime = 0;
     sound[soundList[ev.key]].play();
     console.log(soundList[ev.key]);
 
-    for (i = 1; i <= sciezka.length; i++){ // sprawdza, czy w��czone jest nagrywanie dla ka�dej �cie�ki
+    for (i = 1; i <= sciezka.length; i++){
         if (record[i]) {
-            if (Object.keys(soundList).indexOf(ev.key) > -1) { //je�li naciskany jest poprawny klawisz, dodaje go do sciezki z czasem nagrania
+            if (Object.keys(soundList).indexOf(ev.key) > -1) {
                 sciezka[i].push(
                     {
                         delay: Date.now() - time[i],
@@ -69,15 +69,15 @@ function playKey(ev) { // funkcja grania muzyki
 }
 let timeout = [], koniec = [];
 
-function playRecord(numer_sciezki) { //odtwarzanie sciezki
-    if (!timeout[numer_sciezki] && sciezka[numer_sciezki][0]) { // je�li nie ma timeout dla sciezki, tworzy go dla ka�dego elementu sciezki z odpowiadaj�cym mu op�nieniem.
+function playRecord(numer_sciezki) { 
+    if (!timeout[numer_sciezki] && sciezka[numer_sciezki][0]) { 
         sciezka[numer_sciezki].forEach((ev) => {
             timeout[numer_sciezki] = setTimeout(() => { playKey(ev) }, ev.delay);
         })
         document.getElementById("playButton" + numer_sciezki).innerText = "Stop playing";
-        koniec[numer_sciezki] = setTimeout(() => playRecord(numer_sciezki), sciezka[numer_sciezki][sciezka[numer_sciezki].length - 1].delay + 10) // gdy sko�czy si� sciezka, przycisk automatycznie zmienia si� ponownie na "play"
-    }
-    else { // je�li naci�ni�ty b�dzie przycisk podczas grania sciezki, usuwa si� wszystkie timeouty i przywraca przycisk do "Play"
+        koniec[numer_sciezki] = setTimeout(() => playRecord(numer_sciezki), sciezka[numer_sciezki][sciezka[numer_sciezki].length - 1].delay + 10) 
+       }
+    else {
         clearTimeout(koniec[numer_sciezki]);
         do {
             clearTimeout(timeout[numer_sciezki]);
